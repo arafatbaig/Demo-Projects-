5acca51ff8f712243c39d2ea8f07b56958375a13
Mirza Arafat Baig <103023081+arafatbaig@users.noreply.github.com>
2023-03-25 15:16:54 +0530

    Temperature conversion program

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+document.getElementById("SubmitButton").onclick = function(){
+    let temp ;
+    if(document.getElementById("cButton").checked){
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°C";
+
+    }
+    else if(document.getElemenetById("fButton").checked){
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toFahrenheit(temp);
+        document.getElementById("tempLabel").innerHTML = temp +"°F";
 
 
-const answer = Math.floor(Math.random() * 10 + 1);
-let guesses = 0;
+    }
+    else {
+        document.getElementById("tempLabel").innerHTML = "Select a unit ";
+    }
 
-document.getElementById("submitButton").onclick = function (){
 
-  let guess = document.getElementById("guessField").value
-  guesses+=1;
-  
-  if(guess == answer){
-    alert(`${answer} is the #. It took you ${guesses} guesses`);
-  }
-  else if(guess < answer){
-    alert("Too small!");
-  }
-  else{
-    alert("Too large!");
-  }
 }
+function toCelsius(temp){
+    return (temp - 32) *(5/9);
+}
+function toFahrenheit(temp){
+    return temp *9/5+32;
+}
+
+
 
